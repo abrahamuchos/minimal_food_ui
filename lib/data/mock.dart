70b92acafe384a2ac36cc1926683e3eeffedcf62
abrahamuchos <j.abraham29@gmail.com>
2026-05-04 10:54:17 -0400

    Add fake data, models and products images

---
+++ lib/data/mock.dart
@@ -0,0 +1,196 @@
+import 'package:minimal_food_ui/data/models/product.dart';
+
+class Mock {
+  static final List<Product> products = [
+    Product(
+      title: 'Melting Cheese Pizza',
+      price: '\$10.99',
+      time: '20 min',
+      images: [
+        'assets/images/products/pizza_cheese_1.jpeg',
+        'assets/images/products/pizza_cheese_2.jpeg',
+      ],
+    ),
+    Product(
+      title: 'Double Smash Burger',
+      price: '\$12.50',
+      time: '15 min',
+      images: [
+        'assets/images/products/double_smash_burger_1.jpeg',
+        'assets/images/products/double_smash_burger_2.jpeg',
+      ],
+    ),
+    Product(
+      title: 'Classic Pepperoni',
+      price: '\$9.99',
+      time: '18 min',
+      images: [
+        'assets/images/products/classic_pepperoni_1.jpeg',
+        'assets/images/products/classic_pepperoni_2.jpeg',
+      ],
+    ),
+    Product(
+      title: 'Chicken Caesar Salad',
+      price: '\$9.99',
+      time: '18 min',
+      images: [
+        'assets/images/products/chicken_caesar_salad_1.jpeg',
+        'assets/images/products/chicken_caesar_salad_2.jpeg',
+      ],
+    ),
+    Product(
+      title: 'Sushi Deluxe Set',
+      price: '\$22.00',
+      time: '25 min',
+      images: [
+        'assets/images/products/sushi_deluxe_set_1.jpeg',
+        'assets/images/products/sushi_deluxe_set_2.jpeg',
+      ],
+    ),
+    Product(
+      title: 'Spicy Ramen Bowl',
+      price: '\$11.25',
+      time: '10 min',
+      images: [
+        'assets/images/products/spicy_ramen_bowl_1.jpeg',
+        'assets/images/products/spicy_ramen_bowl_2.jpeg',
+      ],
+    ),
+    Product(
+      title: 'Grilled Salmon',
+      price: '\$15.99',
+      time: '30 min',
+      images: [
+        'assets/images/products/grilled_salmon_1.jpeg',
+      ],
+    ),
+    Product(
+      title: 'Beef Tacos (3pcs)',
+      price: '\$15.99',
+      time: '30 min',
+      images: [
+        'assets/images/products/beef_tacos_1.jpeg',
+      ],
+    ),
+    Product(
+      title: 'Vegetarian Burrito',
+      price: '\$9.00',
+      time: '12 min',
+      images: [
+        'assets/images/products/vegetarian_burrito_1.jpeg',
+        'assets/images/products/vegetarian_burrito_2.jpeg',
+      ],
+    ),
+    Product(
+      title: 'Pasta Carbonara',
+      price: '\$13.40',
+      time: '20 min',
+      images: [
+        'assets/images/products/pasta_carbonara_1.jpeg',
+        'assets/images/products/pasta_carbonara_2.jpeg',
+      ],
+    ),
+    Product(
+      title: 'Chocolate Lava Cake',
+      price: '\$6.50',
+      time: '10 min',
+      images: [
+        'assets/images/products/chocolate_lava_cake_1.jpeg',
+      ],
+    ),
+    Product(
+      title: 'Crispy Chicken Wings',
+      price: '\$11.00',
+      time: '15 min',
+      images: [
+        'assets/images/products/crispy_chicken_wings_1.jpeg',
+        'assets/images/products/crispy_chicken_wings_2.jpeg',
+      ],
+    ),
+    Product(
+      title: 'BBQ Pork Ribs',
+      price: '\$18.50',
+      time: '35 min',
+      images: [
+        'assets/images/products/bbq_pork_ribs_1.jpeg',
+      ],
+    ),
+    Product(
+      title: 'Greek Quinoa Bowl',
+      price: '\$10.20',
+      time: '12 min',
+      images: [
+        'assets/images/products/greek_quinoa_bowl_1.jpeg',
+      ],
+    ),
+    Product(
+      title: 'Mushroom Risotto',
+      price: '\$14.00',
+      time: '25 min',
+      images: [
+        'assets/images/products/mushroom_risotto_1.jpeg',
+        'assets/images/products/mushroom_risotto_2.jpeg',
+      ],
+    ),
+    Product(
+      title: 'Club Sandwich',
+      price: '\$8.75',
+      time: '10 min',
+      images: [
+        'assets/images/products/club_sandwich_1.jpeg',
+        'assets/images/products/club_sandwich_2.jpeg',
+      ],
+    ),
+    Product(
+      title: 'Pad Thai Noodles',
+      price: '\$12.99',
+      time: '18 min',
+      images: [
+        'assets/images/products/pad_thai_noodles_1.jpeg',
+        'assets/images/products/pad_thai_noodles_2.jpeg',
+      ],
+    ),
+    Product(
+      title: 'Falafel Platter',
+      price: '\$9.50',
+      time: '15 min',
+      images: [
+        'assets/images/products/falafel_platter_1.jpeg',
+        'assets/images/products/falafel_platter_2.jpeg',
+      ],
+    ),
+    Product(
+      title: 'Hawaiian Pizza',
+      price: '\$11.50',
+      time: '20 min',
+      images: [
+        'assets/images/products/hawaiian_pizza_1.jpeg',
+        'assets/images/products/hawaiian_pizza_2.jpeg',
+      ],
+    ),
+    Product(
+      title: 'Shrimp Tempura',
+      price: '\$13.00',
+      time: '15 min',
+      images: [
+        'assets/images/products/shrimp_tempura_1.jpeg',
+        'assets/images/products/shrimp_tempura_2.jpeg',
+      ],
+    ),
+    Product(
+      title: 'Strawberry Cheesecake',
+      price: '\$5.99',
+      time: '5 min',
+      images: [
+        'assets/images/products/strawberry_cheesecake_1.jpeg',
+        'assets/images/products/strawberry_cheesecake_2.jpeg',
+      ],
+    ),
+
+
+
+
+
+  ];
+}
+
---
+++ lib/data/models/product.dart
@@ -0,0 +1,13 @@
+class Product {
+  final String title;
+  final String price;
+  final String time;
+  final List<String> images;
+
+  Product({
+    required this.title,
+    required this.price,
+    required this.time,
+    required this.images,
+  });
+}
